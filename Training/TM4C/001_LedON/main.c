b280be3265c7c19d6c6446367a0e6e923fbc8d5d
RishabhBajaj002 <rishabhb2004@gmail.com>
2024-03-21 23:49:55 +0530

    TM4C Led Blink and Patterns
    001_LedOn
    002_LedBlink
    003_LedPattern1
    004_LedCrossPattern1
    005_LedCrossPattern2

--- Training/TM4C/001_LedON/main.c
+++ Training/TM4C/001_LedON/main.c
@@ -1,9 +1,25 @@
-// include the tm4c header
+/**     LED BLINK CODE     **/
+#include "tm4c123gh6pge.h"
+#include <stdint.h>
+volatile long i;
 
-/**
- * main.c
- */
 int main(void)
 {
+    SYSCTL_RCGCGPIO_R |= 0x40;
+    GPIO_PORTG_DIR_R |= 0xFF;
+    GPIO_PORTG_DEN_R |= 0xFF;
+
+    while(1)
+    {
+        GPIO_PORTG_DATA_R &= 0x00;
+        delay(1000000);
+        GPIO_PORTG_DATA_R |= 0xFF;
+        delay(1000000);
+    }
 	return 0;
+}
+
+void delay(int delayTime)
+{
+    for(i=0; i<delayTime; i++);
 }
