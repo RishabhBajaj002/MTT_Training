be0f50bc1a59c3ddce6b96a1dbaa7b345054b4cd
RishabhBajaj002 <rishabhb2004@gmail.com>
2024-03-24 12:39:29 +0530

    Hit Switch Toggle Final
    Hit Switch single toggle with smart delay final code. Delay does not stall code.

--- Training/TM4C/007_2_HitSwitchToggleNew/main.c
+++ Training/TM4C/007_2_HitSwitchToggleNew/main.c
@@ -1,35 +1,36 @@
-#include "tm4c123gh6pge.h"
+/**   Toggles Led using hit switch with smart delay, remaining code is unaffected. Input is positive edge triggered   **/
+#include "tm4c123gh6pge.h"                                  //Including main headers
 #include <stdint.h>
-volatile long timer;
-int regOld, regCurrent;
-int state, firstToggle;
+int oldState, currState, timeCount;                         //Declaring global variables, current and old state describe input state
 
 int main(void)
 {
-    SYSCTL_RCGCGPIO_R |= 0x40;
+    SYSCTL_RCGCGPIO_R |= 0x40;                              //Initializing register states as i/p and o/p and enabling digital values
     GPIO_PORTG_DIR_R |= 0x0F;
     GPIO_PORTG_DEN_R |= 0xFF;
-    GPIO_PORTG_PDR_R |= 0xF0;
-    regOld = 0x00;
+    GPIO_PORTG_PDR_R |= 0xF0;                               //Setting pull-down resistors to input values
+    oldState = 0;                                           //Setting old state as 0 initially, because no input is given
+    timeCount = 1000;                                       //Decides how much smart delay
 
     while(1)
     {
-        regCurrent = GPIO_PORTG_DATA_R;
-        if(((regOld|0x01)==0x01) && (regCurrent|0x01==0x81))
+        if(timeCount==1000)                                 //Only checks for input state change periodically, lets other code run
         {
-            GPIO_PORTG_DATA_R ^= 0x01;
-            regOld = regCurrent;
+            if((GPIO_PORTG_DATA_R&0x80)==0x80)              //If GPIO 7th bit is high -> input is given
+                currState = 1;                              //Sets current state of input as high
+            else
+                currState = 0;                              //Otherwise sets current state as low -> no input has been given
+            timeCount=0;                                    //After checking for input, resets counter and wont check until counter reaches limit(1000)
         }
-        else if ((GPIO_PORTG_DATA_R|0x01)==0x01)
+
+        if((oldState==0) && (currState==1))                 //If old state was low(no press) and current state is high(pressed)
         {
-            regOld = 0x00;
+            GPIO_PORTG_DATA_R ^= 0x01;                      //Change state of led using XOR, low->high or high->low
         }
-        delay(700);
+        oldState=currState;                                 //Makes the old state current one after toggle->wont toggle until next rising edge
+        timeCount++;                                        //Increments timeCount in main code, it will run continuously and wont check i/p until limit
+
+        //Any code here will run with every iteration of while(1) loop and wont stop or be skipped even in case of checking i/p or toggle
     }
     return 0;
-}
-
-void delay(int delayTime)
-{
-    for(timer=0; timer<delayTime; timer++);
 }
